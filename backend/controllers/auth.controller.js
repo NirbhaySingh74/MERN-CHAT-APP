336ab5469c8b17d7d08b718291f4d35b632c6f12
Nirbhay <nirbhaysingh943012@gmail.com>
2024-06-16 19:18:38 +0530

    Logout code completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -62,11 +62,17 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.log("Error in signup controller", error.message);
+    console.log("Error in Login controller", error.message);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
-export const logout = async (req, res) => {
-  res.json({ name: "Nirbhay Singh logout successfully" });
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in Logout controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
