0f0087f53679a0bde06053567c0fa787de54d055
Nirbhay <nirbhaysingh943012@gmail.com>
2024-06-16 18:07:47 +0530

    userSignUp controller code completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,36 @@
+import { User } from "../models/user.models.js";
+import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
   try {
-    const { name, email } = req.body;
-    console.log(name, email);
-    return res.status(200).json({ name: name, email: email });
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password don't match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Userame already Exist" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    const newuser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newuser.save();
+    return res.status(201).json({ newuser });
   } catch (error) {
-    return res.status(400).json({ error: "signup error" });
+    console.log("Error in signup controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
