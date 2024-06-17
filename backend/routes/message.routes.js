6563f3c1213f0dd20d0ea58d8d2be61e72aaeb79
Nirbhay <nirbhaysingh943012@gmail.com>
2024-06-17 08:50:12 +0530

    protectRoute Added

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { sendMessage } from "../controllers/message.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
 
 const route = express.Router();
 
-route.post("/send/:id", sendMessage);
+route.post("/send/:id", protectRoute, sendMessage);
 export default route;
