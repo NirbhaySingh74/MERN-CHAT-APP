8c1ddb913e092ba642aa2a00e740f8a5da5e199c
Nirbhay <nirbhaysingh943012@gmail.com>
2024-06-17 13:50:31 +0530

    get message code completed!!

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { getMessages, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const route = express.Router();
 
+route.get("/:id", protectRoute, getMessages);
 route.post("/send/:id", protectRoute, sendMessage);
 export default route;
