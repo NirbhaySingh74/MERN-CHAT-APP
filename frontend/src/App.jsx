2d69d5e2b7e1aeaaf761267a6fcfc6cf04fee191
Nirbhay <nirbhaysingh943012@gmail.com>
2024-06-17 20:20:08 +0530

    signup page UI copleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+// import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Login />
+      {/* <Login /> */}
+      <Signup />
     </div>
   );
 }
