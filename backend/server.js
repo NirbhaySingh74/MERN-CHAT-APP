cd4c15f2b54ffd1ac5bdf000f5c32651c66ca13c
Nirbhay <nirbhaysingh943012@gmail.com>
2024-06-23 18:13:39 +0530

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -14,9 +15,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 dotenv.config();
 const PORT = process.env.PORT || 7000;
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -31,3 +30,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
